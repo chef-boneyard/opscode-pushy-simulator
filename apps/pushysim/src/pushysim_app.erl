f7c20937543d6f53da140ac15fe3d2182e9c1ff4
jamesc <james@opscode.com>
2012-09-20 16:42:21 -0700

    Add default for zmq_io_processes env

--- apps/pushysim/src/pushysim_app.erl
+++ apps/pushysim/src/pushysim_app.erl
@@ -21,7 +21,7 @@
 
 start(_StartType, _StartArgs) ->
     error_logger:info_msg("Pushy Client Simulator starting.~n"),
-    IoProcesses = pushy_util:get_env(pushysim, zmq_io_processes, fun is_integer/1),
+    IoProcesses = pushy_util:get_env(pushysim, zmq_io_processes, 1, fun is_integer/1),
     case erlzmq:context(IoProcesses) of
         {ok, Ctx} ->
             case pushysim_sup:start_link(#client_state{ctx=Ctx}) of
